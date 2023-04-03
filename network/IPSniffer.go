e683a6a8852a7fcbb86b1d2dcf23d5a4695a70ad
Albertchamberlain <albert7king@gmail.com>
2023-04-03 14:20:15 +0800

    ✨ <add>:<IPSniffer>

---
+++ network/IPSniffer.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		panic(err)
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Println(ipnet.IP.String())
+			}
+		}
+	}
+}
